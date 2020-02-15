95a662627f7f24935e603e0e53cf430568e1f900
Jaehyun <heroinspace@naver.com>
2020-02-15 16:44:29 +0900

    #2.3

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-console.log("Hi!");
+const express = require("express");
+const app = express();
+const port = 4000;
+
+function handleListening() {
+  console.log(`Listening on: http://localhost:${port}`);
+}
+
+app.listen(port, handleListening);
