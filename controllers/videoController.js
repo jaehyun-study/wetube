92a169932a60a84925ae4cd3a945de1d1e5df8a2
Jaehyun <heroinspace@naver.com>
2020-02-25 23:56:19 +0900

    #2.18

--- controllers/videoController.js
+++ controllers/videoController.js
@@ -1,7 +1,18 @@
 export const home = (req, res) => res.render("Home", { pageTitle: "Home" });
-export const search = (req, res) => res.render("Search");
+
+export const search = (req, res) => {
+  const {
+    query: { term: searchTerm }
+  } = req;
+  res.render("Search", { searchTerm });
+};
+
 export const videos = (req, res) => res.render("Videos");
+
 export const upload = (req, res) => res.render("Upload");
+
 export const videoDetail = (req, res) => res.render("Video Detail");
+
 export const editVideo = (req, res) => res.render("Edit Video");
+
 export const deleteVideo = (req, res) => res.render("Delete Video");
