a1cb687f7a80ea5c1bf363b4758ad524a3307677
Jaehyun <heroinspace@naver.com>
2020-02-28 00:39:33 +0900

    #2.21

--- controllers/videoController.js
+++ controllers/videoController.js
@@ -1,4 +1,8 @@
-export const home = (req, res) => res.render("Home", { pageTitle: "Home" });
+import { videos } from "../db";
+
+export const home = (req, res) => {
+  res.render("Home", { pageTitle: "Home", videos });
+};
 
 export const search = (req, res) => {
   const {
@@ -7,12 +11,10 @@ export const search = (req, res) => {
   res.render("Search", { searchTerm });
 };
 
-export const videos = (req, res) => res.render("Videos");
+export const upload = (req, res) => res.render("upload");
 
-export const upload = (req, res) => res.render("Upload");
+export const videoDetail = (req, res) => res.render("videoDetail");
 
-export const videoDetail = (req, res) => res.render("Video Detail");
+export const editVideo = (req, res) => res.render("editVideo");
 
-export const editVideo = (req, res) => res.render("Edit Video");
-
-export const deleteVideo = (req, res) => res.render("Delete Video");
+export const deleteVideo = (req, res) => res.render("deleteVideo");
