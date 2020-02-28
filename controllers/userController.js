4b881565efbd38bebd4dac8199906750efc00d53
Jaehyun <heroinspace@naver.com>
2020-02-28 17:23:39 +0900

    #2.24

--- controllers/userController.js
+++ controllers/userController.js
@@ -19,7 +19,14 @@ export const postJoin = (req, res) => {
   }
 };
 
-export const login = (req, res) => res.render("login");
+export const getLogin = (req, res) => {
+  res.render("login", { pageTitle: "Login" });
+};
+
+export const postLogin = (req, res) => {
+  res.redirect(routes.home);
+};
+
 export const logout = (req, res) => res.render("logout");
 export const users = (req, res) => res.render("users");
 export const userDetail = (req, res) => res.render("userDetail");
