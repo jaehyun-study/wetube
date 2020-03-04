c1791a60591f9589b479d81df54010305e2471d5
Jaehyun <heroinspace@naver.com>
2020-03-05 00:19:15 +0900

    #3.7

--- app.js
+++ app.js
@@ -18,6 +18,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(morgan("dev"));
 app.use(localsMiddleware);
+app.use("/uploads", express.static("uploads"));
 app.use(routes.home, globalRouter);
 app.use(routes.users, userRouter);
 app.use(routes.videos, videoRouter);
