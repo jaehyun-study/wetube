8bbb34dd8713ff5a329483d5a9a7c0d7dd031444
Jaehyun <heroinspace@naver.com>
2020-03-11 12:41:39 +0900

    #6.3

--- app.js
+++ app.js
@@ -3,11 +3,13 @@ import helmet from "helmet";
 import morgan from "morgan";
 import cookieParser from "cookie-parser";
 import bodyParser from "body-parser";
+import passport from "passport";
 import { localsMiddleware } from "./middlewares";
 import userRouter from "./routers/userRouter";
 import videoRouter from "./routers/videoRouter";
 import globalRouter from "./routers/globalRouter";
 import routes from "./routes";
+import "./passport";
 
 const app = express();
 
@@ -17,6 +19,8 @@ app.use(cookieParser());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(morgan("dev"));
+app.use(passport.initialize());
+app.use(passport.session());
 app.use(localsMiddleware);
 app.use("/uploads", express.static("uploads"));
 app.use("/static", express.static("static"));
--- controllers/userController.js
+++ controllers/userController.js
@@ -1,12 +1,12 @@
+import passport from "passport";
 import routes from "../routes";
 import User from "../models/User";
-import passport from "passport";
 
 export const getJoin = (req, res) => {
   res.render("join", { pageTitle: "Join" });
 };
 
-export const postJoin = async (req, res) => {
+export const postJoin = async (req, res, next) => {
   const {
     body: { name, email, password, password2 }
   } = req;
@@ -20,10 +20,11 @@ export const postJoin = async (req, res) => {
         email
       });
       await User.register(user, password);
+      next();
     } catch (error) {
       console.log(error);
+      res.redirect(routes.home);
     }
-    res.redirect(routes.home);
   }
 };
 
@@ -31,9 +32,10 @@ export const getLogin = (req, res) => {
   res.render("login", { pageTitle: "Login" });
 };
 
-export const postLogin = (req, res) => {
-  res.redirect(routes.home);
-};
+export const postLogin = passport.authenticate("local", {
+  successRedirect: routes.home,
+  failureRedirect: routes.login
+});
 
 export const logout = (req, res) => {
   //todo: processing logout
