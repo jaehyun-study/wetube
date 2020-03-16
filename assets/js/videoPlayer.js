1f7315075bfaf4ad6b159ef6542a6f537aec6849
Jaehyun <heroinspace@naver.com>
2020-03-16 13:54:53 +0900

    #8.3

--- assets/js/videoPlayer.js
+++ assets/js/videoPlayer.js
@@ -2,6 +2,7 @@ const videoContainer = document.getElementById("jsVideoPlayer");
 const videoPlayer = document.querySelector("#jsVideoPlayer video");
 const playButton = document.getElementById("jsPlayButton");
 const volumeButton = document.getElementById("jsVolumeButton");
+const fullScreenButton = document.getElementById("jsFullScreenButton");
 
 if (videoContainer) {
   init();
@@ -10,6 +11,7 @@ if (videoContainer) {
 function init() {
   playButton.addEventListener("click", handlePlayButtonClick);
   volumeButton.addEventListener("click", handleVolumeButtonClick);
+  fullScreenButton.addEventListener("click", enterFullScreen);
 }
 
 function handlePlayButtonClick() {
@@ -30,4 +32,18 @@ function handleVolumeButtonClick() {
     videoPlayer.muted = true;
     volumeButton.innerHTML = '<i class="fas fa-volume-mute"></i>';
   }
+}
+
+function enterFullScreen() {
+  videoContainer.webkitRequestFullScreen();
+  fullScreenButton.innerHTML = '<i class="fas fa-compress"></i>';
+  fullScreenButton.removeEventListener("click", enterFullScreen);
+  fullScreenButton.addEventListener("click", exitFullScreen);
+}
+
+function exitFullScreen() {
+  document.webkitExitFullscreen();
+  fullScreenButton.innerHTML = '<i class="fas fa-expand"></i>';
+  fullScreenButton.removeEventListener("click", exitFullScreen);
+  fullScreenButton.addEventListener("click", enterFullScreen);
 }
