6cd7022dae896ab38839d70cfc7b7852df2f8bbc
Jaehyun <heroinspace@naver.com>
2020-03-26 17:55:32 +0900

    #css

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -4,7 +4,9 @@ import Comment from "../models/Comment";
 
 export const home = async (req, res) => {
   try {
-    const videos = await Video.find({}).sort({ _id: -1 });
+    const videos = await Video.find({})
+      .sort({ _id: -1 })
+      .populate("creator");
     res.render("home", { pageTitle: "Home", videos });
   } catch (error) {
     console.log(error);
@@ -18,7 +20,9 @@ export const search = async (req, res) => {
   } = req;
   let videos = [];
   try {
-    videos = await Video.find({ title: { $regex: searchTerm, $options: "i" } });
+    videos = await Video.find({
+      title: { $regex: searchTerm, $options: "i" }
+    }).populate("creator");
   } catch (error) {
     console.log(error);
   }
